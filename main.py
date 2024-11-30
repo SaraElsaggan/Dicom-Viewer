34f412f4e938a838f2af62fa8d0b1fd3765ecec3
SaraElsaggan <sara.mahmoud.sm600@gmail.com>
2024-11-30 21:26:54 +0200

    vol_render_from_GPT_works

--- main.py
+++ main.py
@@ -1,119 +1,90 @@
-from PyQt5.QtWidgets import  QApplication, QMainWindow, QShortcut, QFileDialog , QSplitter , QFrame , QSlider
-import sys
-from PyQt5.QtGui import QIcon, QKeySequence
-from mainwindow import Ui_MainWindow  
-import numpy as np
-import pandas as pd
-from scipy.io import wavfile
-import pyqtgraph as pg
-from scipy.fftpack import rfft, rfftfreq, irfft , fft , fftfreq
-from PyQt5.QtCore import pyqtSlot
-# from PyQt5.QtOpenGL import QOpenGLWidget
-
+from PyQt5.QtWidgets import QApplication, QMainWindow, QShortcut, QFileDialog
+from PyQt5.QtGui import QKeySequence
+from mainwindow import Ui_MainWindow  # Ensure this is your generated UI file
 from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 import vtk
-from PyQt5.QtGui import QImage, QPixmap
-from PyQt5.QtWidgets import QLabel
-from vtkmodules.util.numpy_support import vtk_to_numpy
 import pydicom
-import numpy as np
-# from PyQt5.QtOpenGL import QOpenGLWidget
-from OpenGL.GL import *
-from OpenGL.GLUT import *
+import os
 
 
- 
-class MyWindow(QMainWindow):   
-    
-    def __init__(self ):
-        super(MyWindow , self).__init__()
-      
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+
+        # Setup the UI and VTK renderer
         self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)  
+        self.ui.setupUi(self)
 
-        
-        self.vtk_widget = QVTKRenderWindowInteractor(self.ui.openGLWidget)
+        # Initialize VTK render window
+        self.vtk_widget = QVTKRenderWindowInteractor(self.ui.widget)
         self.renderer = vtk.vtkRenderer()
         self.vtk_widget.GetRenderWindow().AddRenderer(self.renderer)
         self.vtk_widget.Initialize()
-    
-            
-        self.image_data = np.array([])
-        self.image_position = np.array([])
-        self.pixel_spacing = np.array([])
-        self.surface_actor = None
-    
-        # some shortcuts
+
+        # Add shortcut for loading files
         QShortcut(QKeySequence("Ctrl+o"), self).activated.connect(self.load_file)
-        
 
     def load_file(self):
-        # Open a dialog to select a DICOM series directory
-        Directory = QFileDialog.getExistingDirectory(self, "Open DICOM Series Directory", "", options=QFileDialog.Options())
-        if Directory:
-            # Load and render the DICOM series
-            self.load_dicom_series(Directory)
+        """Load a folder of DICOM files and display it in 3D."""
+        folder = QFileDialog.getExistingDirectory(self, "Select DICOM Folder")
+        if not folder:
+            return
 
-    # Function to load and render a DICOM series
-    def load_dicom_series(self, directory):
-        # Clear previous rendering
-        if self.surface_actor:
-            self.renderer.RemoveActor(self.surface_actor)
-        self.renderer.RemoveAllViewProps()
+        # Load all DICOM files in the folder
+        reader = vtk.vtkDICOMImageReader()
+        reader.SetDirectoryName(folder)
+        reader.Update()
 
-        # Read DICOM files in a series using VTK DICOM reader
-        dicom_reader = vtk.vtkDICOMImageReader()
-        dicom_reader.SetDirectoryName(directory)
-        dicom_reader.Update()
-        self.image_data = dicom_reader.GetOutput()
+        # Extract volume data from the reader
+        volume = vtk.vtkImageData()
+        volume.DeepCopy(reader.GetOutput())
 
-        # Set up and render the surface representation of the DICOM data
-        self.setup_surface_rendering()
-        self.vtk_widget.GetRenderWindow().Render()
+        # Create a volume mapper
+        volume_mapper = vtk.vtkGPUVolumeRayCastMapper()
+        volume_mapper.SetInputData(volume)
 
-    # Function to set up surface rendering using marching cubes
-    def setup_surface_rendering(self):
-        # Apply marching cubes algorithm to create a surface representation
-        marching_cubes = vtk.vtkMarchingCubes()
-        marching_cubes.SetInputData(self.image_data)
-        marching_cubes.SetValue(0, 0)
-        
-        # Create a mapper and actor for the surface representation
-        mapper = vtk.vtkPolyDataMapper()
-        mapper.SetInputConnection(marching_cubes.GetOutputPort())
-        self.surface_actor = vtk.vtkActor()
-        self.surface_actor.SetMapper(mapper)
-        
-        # Add the surface actor to the renderer and adjust the camera
-        self.renderer.AddActor(self.surface_actor)
+        # Create a volume property
+        volume_property = vtk.vtkVolumeProperty()
+        volume_property.ShadeOn()
+        volume_property.SetInterpolationTypeToLinear()
+
+        # Set up transfer functions for color and opacity
+        color_func = vtk.vtkColorTransferFunction()
+        color_func.AddRGBPoint(0, 0.0, 0.0, 0.0)
+        color_func.AddRGBPoint(500, 1.0, 0.5, 0.3)
+        color_func.AddRGBPoint(1000, 1.0, 1.0, 0.9)
+        color_func.AddRGBPoint(1150, 1.0, 1.0, 1.0)
+
+        opacity_func = vtk.vtkPiecewiseFunction()
+        opacity_func.AddPoint(0, 0.0)
+        opacity_func.AddPoint(500, 0.2)
+        opacity_func.AddPoint(1000, 0.4)
+        opacity_func.AddPoint(1150, 0.8)
+
+        volume_property.SetColor(color_func)
+        volume_property.SetScalarOpacity(opacity_func)
+
+        # Create the volume
+        volume_actor = vtk.vtkVolume()
+        volume_actor.SetMapper(volume_mapper)
+        volume_actor.SetProperty(volume_property)
+
+        # Add the volume to the renderer
+        self.renderer.AddVolume(volume_actor)
+        self.renderer.SetBackground(0.1, 0.1, 0.1)  # Set background color
         self.renderer.ResetCamera()
-        
-        # Update the rendering window
+
+        # Render the scene
         self.vtk_widget.GetRenderWindow().Render()
 
-    # Function to handle surface rendering checkbox state change
-    def surface_rendering(self):
-        if self.ui.radioButton_3.isChecked():
-            # Clear previous rendering and set up surface rendering
-            self.renderer.RemoveAllViewProps()
-            self.setup_surface_rendering()
-            self.vtk_widget.GetRenderWindow().Render()
-
- 
-    # Function to handle surface rendering checkbox state 
-
- 
 
 def main():
-    app = QApplication(sys.argv)
-    window = MyWindow() 
-   
-   
+    app = QApplication([])
+    window = MyWindow()
     window.showMaximized()
     window.show()
-    
-    sys.exit(app.exec_())
-    
+    app.exec_()
+
 
 if __name__ == '__main__':
     main()
